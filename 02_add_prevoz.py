33f9ab2672efad05cae3ebf921c88d126c47b15f
Jure Cuhalev <gandalfar@gmail.com>
2013-07-06 10:51:59 +0200

    delete sample ride after creating it

--- 02_add_prevoz.py
+++ 02_add_prevoz.py
@@ -2,6 +2,7 @@ from oauth2client.file import Storage
 from datetime import date
 from urllib import urlencode
 import httplib2
+import json
 
 storage = Storage('credentials.json')
 credentials = storage.get()
@@ -10,10 +11,11 @@ credentials = storage.get()
 http = httplib2.Http()
 http = credentials.authorize(http)
 
-resp, content = http.request("https://prevoz.org/api/accounts/status/", 'GET')
+print "\n\nRead our status - username and if we're authenticated:"
+resp, content = http.request("https://prevoz.org/api/accounts/status/", "GET")
 print content
 
-
+print "\n\nCreate a new carshare:\n"
 data = {'transptype': 0,
         'transpfrom': 'Ljubljana',
         'transpfromcountry': 'SI',
@@ -27,5 +29,12 @@ data = {'transptype': 0,
         'transpdescr': 'testni prevoz'
     	}
 
-resp, content = http.request("https://prevoz.org/api/carshare/create/", 'POST', urlencode(data))
+resp, content = http.request("https://prevoz.org/api/carshare/create/", "POST", urlencode(data))
+print content
+data = json.loads(content)
+carshare_id = data["id"]
+
+print "\n\nDelete this carshare (since it's a live site and we don't want to dirty it)\n"
+
+resp, content = http.request("https://prevoz.org/api/carshare/delete/%s" % carshare_id, "POST")
 print content
